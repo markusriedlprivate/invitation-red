0aa39cd7d3949f3ff26fbffcb4cfccf0e145237c
MarkusRiedl <markus.riedl@up2parts.com>
2023-12-01 12:09:46 +0100

    removed uncompleted function

--- source/index.js
+++ source/index.js
@@ -1,60 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = require("crypto");
 const fs = require("fs");
 const https = require("https");
 const path = require("path");
-const zlib = require("zlib");
 console.log("Here we go!");
-const decryptFile = () => {
-    // Datei- und Schlüsselpfade
-    const encryptedFilePath = "secret.enc";
-    const decryptedFilePath = "entschluesselte_datei.zip";
-    const keyPath = "secret.key";
-    const ivPath = "iv.txt";
-    // Schlüssel und IV einlesen
-    const keyFileContent = fs.readFileSync(keyPath, "utf8");
-    // Entferne alle ungültigen Zeichen aus dem Schlüssel
-    const validHexChars = /^[0-9a-fA-F]+$/;
-    let cleanedKeyContent = "";
-    for (const char of keyFileContent) {
-        if (validHexChars.test(char)) {
-            cleanedKeyContent += char;
-        }
-    }
-    const key = cleanedKeyContent.slice(0, 32);
-    // Überprüfe, ob der bereinigte Schlüssel die richtige Länge hat
-    if (key.length !== 32) {
-        console.error("Ungültige Schlüssellänge nach Bereinigung.");
-        process.exit(1);
-    }
-    if (!validHexChars.test(key)) {
-        console.error("Ungültige Zeichen im Schlüssel.");
-        process.exit(1);
-    }
-    else
-        console.log("all valid");
-    const ivBuffer = Buffer.from(fs.readFileSync(ivPath));
-    // AES256-GCM entschlüsseln
-    const decipher = crypto.createDecipheriv("aes-256-gcm", key, ivBuffer);
-    const encryptedContent = fs.readFileSync(encryptedFilePath);
-    const blockSize = 16; // Beispielblockgröße
-    let decryptedContent = Buffer.alloc(0);
-    for (let i = 0; i < encryptedContent.length; i += blockSize) {
-        const chunk = encryptedContent.slice(i, i + blockSize);
-        decryptedContent = Buffer.concat([
-            decryptedContent,
-            decipher.update(chunk),
-        ]);
-    }
-    decryptedContent = Buffer.concat([decryptedContent, decipher.final()]);
-    // Entpacken (dekomprimieren) des ZIP-Archivs
-    const decompressedContent = zlib.unzipSync(decryptedContent);
-    // Entschlüsselte und entpackte Daten in eine Datei schreiben
-    fs.writeFileSync(decryptedFilePath, decompressedContent, "binary");
-    console.log("Entschlüsselung und Entpackung abgeschlossen.");
-};
-decryptFile();
 const clearSmallerTxtPath = process.env.PWD + path.sep + "clear_smaller.txt";
 const readTxtFile = (path) => {
     return fs.readFileSync(path).toString();
